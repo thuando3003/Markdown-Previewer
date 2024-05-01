efa24675599f679f9953f7ed02cebe57e401e2ce
Đỗ Minh Thuận <96808038+21012324ThuanDM@users.noreply.github.com>
2024-05-01 12:47:33 +0700

    add marked lib

--- Markdown Previewer.js
+++ Markdown Previewer.js
@@ -1,15 +1,10 @@
-function convertMarkdownToHTML(markdown) {
-    markdown = markdown.replace(/^#\s(.*)$/gm, '<h1>$1</h1>');
-    markdown = markdown.replace(/^##\s(.*)$/gm, '<h2>$1</h2>');
-    markdown = markdown.replace(/^###\s(.*)$/gm, '<h3>$1</h3>');
-    return markdown;
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const editorContent = document.getElementById("editorContent");
+    const previewerContent = document.getElementById("previewerContent");
 
-document.addEventListener('DOMContentLoaded', function() {
-    const editorContent = document.getElementById('editorContent');
-    const previewerContent = document.getElementById('previewerContent');
-    document.getElementById('editor').addEventListener('input', function() {
-        const markdown = editorContent.innerText;
-        previewerContent.innerHTML = convertMarkdownToHTML(markdown);
+    editorContent.addEventListener("input", function() {
+        const markdownText = editorContent.innerText;
+        const htmlText = marked(markdownText);
+        previewerContent.innerHTML = htmlText;
     });
 });
